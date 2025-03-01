118e96f805c85d29c398e3bca38f2dd861518da0
Vlad <laushvl@gmail.com>
2025-03-01 16:02:47 +0400

    Fourth task complete

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,29 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+  if ("marks" in this) {
+    this.marks.push(...marksToAdd);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if ("marks" in this) {
+        return this.marks.reduce((average, mark, index, array) => average + mark / array.length, 0);
+      }
+    return 0;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
 }
